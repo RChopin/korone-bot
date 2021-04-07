ad65d4c415936d79c1733ffa290edfbf9ef21d22
Chop <radoslaw.wojs@gmail.com>
2021-04-07 11:03:14 +0200

    🚀Initial

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Client } = require("dogehouse.js");
+require("dotenv").config();
+const app = new Client();
+app
+  .connect(process.env.DOGEHOUSE_TOKEN, process.env.DOGEHOUSE_REFRESH_TOKEN)
+  .then(async () => {
+    console.log("Bot connected!");
+    console.log(await app.rooms.top); // Log all of your top rooms to console
+  });
